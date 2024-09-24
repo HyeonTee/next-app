4cbab4768b972f38236017b763101227cb1e351f
HyeonTee <gusxo1820@icloud.com>
2024-09-24 18:29:42 +0900

    chore: Modify UI

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,36 @@
-import { Box, Container, Heading, useColorModeValue } from '@chakra-ui/react'
+import { Box, Container, Flex, useColorModeValue } from '@chakra-ui/react'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <Container>
-      <Heading>Home</Heading>
-      <Image
-        src="/dev_ggamja.jpg"
-        alt="ggamja"
-        width="1000"
-        height="1000"
-      />
+      <Flex
+        mt={4}
+        mb={4} // 아래쪽 여백을 추가하여 Box와 텍스트 사이 간격 설정
+        justify="center" // 수평 중앙 정렬
+        align="flex-start" // 수직 시작점에 맞추기
+        w="100%" // Flex의 너비를 100%로 설정
+      >
+      <Box
+        borderColor="whiteAlpha.800"
+        borderWidth={4}
+        borderStyle="solid"
+        w="400px" // 너비를 400px로 조정
+        h="400px" // 높이를 400px로 조정
+        display="inline-block"
+        borderRadius="full"
+        overflow="hidden"
+        position="relative" // position을 relative로 설정
+      >
+        <Image
+          src="/dev_ggamja.jpg"
+          alt="ggamja"
+          layout="fill" // Box에 맞게 이미지 크기 조정
+          style={{ objectFit: 'cover' }} // CSS 스타일로 objectFit 설정
+        />
+      </Box>
+      </Flex>
+
       <Box borderRadius="lg"
            mb={6}
            p={3}
