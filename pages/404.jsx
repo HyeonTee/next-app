173afc1236aa29e211e8894325530993ebbf9f71
HyeonTee <gusxo1820@icloud.com>
2024-09-24 16:56:01 +0900

    chore: Add motion to 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -5,22 +5,61 @@ import {
   Text,
   Container,
   Divider,
-  Button
+  Button,
+  Icon,
+  useColorModeValue
 } from '@chakra-ui/react'
+import { FaHome } from 'react-icons/fa'
+import { motion } from 'framer-motion'
 
 const NotFound = () => {
+  const bg = useColorModeValue('gray.100', 'gray.700');
+  const textColor = useColorModeValue('gray.800', 'whiteAlpha.900');
+
   return (
-    <Container>
-      <Heading as="h1">Not found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
+    <Container
+      maxW="container.md"
+      textAlign="center"
+      bg={bg}
+      p={8}
+      borderRadius="lg"
+      shadow="xl"
+    >
+      <motion.div
+        initial={{ scale: 0.9, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Heading as="h1" size="2xl" mb={4} color={textColor}>
+          404 Not found
+        </Heading>
+        <Text fontSize="xl" color={textColor} mb={4}>
+          페이지를 찾을 수 없습니다.
+        </Text>
+      </motion.div>
+
       <Divider my={6} />
-      <Box my={6} align="center">
-        <Button as={NextLink} href="/" colorScheme="teal">
-          Return to home
-        </Button>
-      </Box>
+
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.7 }}
+      >
+        <Box my={6} align="center">
+          <Button
+            as={NextLink}
+            href="/"
+            colorScheme="teal"
+            leftIcon={<Icon as={FaHome} />}
+            size="lg"
+            shadow="md"
+          >
+            Return to home
+          </Button>
+        </Box>
+      </motion.div>
     </Container>
   )
 }
 
-export default NotFound
+export default NotFound;
