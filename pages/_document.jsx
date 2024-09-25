e52e980888318c5ddab837301a67aa156eb611e3
HyeonTee <gusxo1820@icloud.com>
2024-09-25 15:35:37 +0900

    feat: Set title

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import theme from '../lib/theme'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Hyeontae&apos;s Homepage</title>
+        </Head>
+        <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
-      </body>
-    </Html>
-  );
+        </body>
+      </Html>
+    )
+  }
 }
