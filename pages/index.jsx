4c8529508674a2a17c72cebfa3690bab301a87af
HyeonTee <gusxo1820@icloud.com>
2024-09-25 13:34:55 +0900

    feat: Create typing effect on greeting message

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,35 @@
 import { Box, Container, useColorModeValue } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const greeting = ' 안녕하세요, 백엔드 개발자 김현태입니다.'
+  const [displayedGreeting, setDisplayedGreeting] = useState('')
+
+  useEffect(() => {
+    let index = 0
+    const typingInterval = setInterval(() => {
+      if (index < greeting.length) {
+        setDisplayedGreeting((prev) => prev + greeting.charAt(index))
+        index++
+      } else {
+        clearInterval(typingInterval)
+      }
+    }, 100)
+
+    return () => clearInterval(typingInterval)
+  }, [greeting])
+
   return (
     <Container>
-      <Box borderRadius="lg"
-           mb={6}
-           p={3}
-           textAlign="center"
-           bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-           css={{ backdropFilter: 'blur(10px)' }}
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
       >
-        안녕하세요, 개발자 김현태입니다.
+        {displayedGreeting}
       </Box>
     </Container>
   )
